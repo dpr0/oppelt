1f1b98dea08cb9555ff7778d0317c250a4d998f0
dvitvitskiy <dvitvitskiy@npoprogress.com>
2020-11-12 22:48:10 +0300

    init

---
+++ Gemfile
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+ruby '2.6.3'
+
+source 'https://rubygems.org'
+git_source(:github) { |repo| "https://github.com/#{repo}.git" }
+
+gem 'activerecord-import'
+gem 'bootsnap'
+gem 'pg'
+gem 'rails'
+gem 'slim-rails'
+gem 'uglifier'
+gem 'webpacker'
+gem 'listen'
+gem 'dotenv-rails'
+gem 'puma'
+
+group :development do
+  gem 'byebug'
+  gem 'capistrano',            require: false
+  gem 'capistrano-bundler',    require: false
+  gem 'capistrano-nvm',        require: false
+  gem 'capistrano-rails',      require: false
+  gem 'capistrano-rbenv',      require: false
+  gem 'capistrano-yarn',       require: false
+  gem 'capistrano3-puma',      require: false
+  gem 'ed25519'
+  gem 'bcrypt_pbkdf'
+end
---
+++ db/schema.rb
@@ -0,0 +1,65 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_09_27_000000) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "code"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "item_type"
+    t.string "name"
+    t.string "value"
+    t.integer "qty"
+    t.integer "price"
+    t.integer "category_id"
+    t.integer "manufacturer_id"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "manufacturers", force: :cascade do |t|
+    t.string "code"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "uid"
+    t.string "name"
+    t.string "email"
+    t.string "phone"
+    t.string "provider"
+    t.string "token"
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
